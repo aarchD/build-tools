ffa4a3b97a9f6a519814d3c6205acfc495dc9cd7
Deepak Kumar <notwho53@gmail.com>
2025-05-10 20:20:27 +0530

    pkgmeta: drop buildc
    no need
    
    Signed-off-by: Deepak Kumar <notwho53@gmail.com>

--- pkgmeta.sh
+++ pkgmeta.sh
@@ -2,26 +2,11 @@
 
 set -euo pipefail
 
-# Usage: pkgmeta.sh [gen|buildc|check]
+# Usage: pkgmeta.sh [gen|check]
 
 case "$1" in
   gen)
     output_file="metadata"
-    > "$output_file"
-    for dir in */; do
-      pkg="${dir%/}"
-      combined_hashes=""
-      while IFS= read -r -d '' file; do
-        hash=$(sha256sum "$file" | awk '{print $1}')
-        combined_hashes+="$hash"
-      done < <(find "$dir" -type f -print0 | sort -z)
-      sum=$(printf "%s" "$combined_hashes" | sha256sum | awk '{print $1}')
-      echo "$pkg - $sum" >> "$output_file"
-    done
-    ;;
-
-  buildc)
-    output_file="build-metadata"
     > "$output_file"
     for dir in */; do
       pkg="${dir%/}"
@@ -45,7 +30,7 @@ case "$1" in
     ;;
 
   *)
-    echo "Usage: $0 [gen|buildc|check]"
+    echo "Usage: $0 [gen|check]"
     exit 1
     ;;
 esac
